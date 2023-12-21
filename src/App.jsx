fb51054ee34966d328bf2b962dbc22d2af251be7
Antu Paul <antupaul81@gmail.com>
2023-12-22 01:59:20 +0600

    store and component (header, footer)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
+import { Header, Footer } from "./components/index"
+import { Outlet } from "react-router-dom"
 
 
 function App() {
-  
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+    const [loading, setLoading] = useState(true)
+    const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A blog with appwrite</h1>
-    </>
-  )
+
+    useEffect(() => {
+      authService.getCurrentUser()
+        .then((userData) => {
+          if (userData) {
+            dispatch(login({userData}))
+          }else{
+            dispatch(logout())
+          }
+        })
+        .finally(() => setLoading(false))
+    }, [])
+
+  
+
+
+
+    if(!loading){
+      return (
+        <>
+          <Header />
+          <main>
+            {/* < Outlet /> */}
+          </main>
+          <Footer />
+        </>
+      )
+    }else{
+      return (
+        <>
+          <h1>from else condition</h1>
+        </>
+      )
+    }
 }
 
 export default App
