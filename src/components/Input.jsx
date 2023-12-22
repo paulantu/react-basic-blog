4852784488865d7bafa7951f5bcb1056412d7bca
Antu Paul <antupaul81@gmail.com>
2023-12-22 22:20:00 +0600

    component (logout, logo, button)

---
+++ src/components/Input.jsx
@@ -0,0 +1,32 @@
+import React, {useId} from 'react'
+
+
+const Input = React.forwardRef(({
+    label,
+    type = 'text',
+    className = '',
+    ...props
+}, ref) => {
+
+    const id = useId();
+
+
+    return (
+        <div className='w-full'>
+            {label && <label className={`block mb-1 ${classname}`} htmlFor={id}>{label}</label>}
+
+            <input 
+                type={type}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-200 ${className}`}
+                ref={ref}
+                {...props}
+                id={id}
+            />
+        </div>
+    )
+})
+
+
+
+
+export default Input
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,14 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from './Container/Container'
+import Logo from "./Logo/Logo";
+import LogoutButton from "./Header/LogoutButton";
 
 
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo
 } 
